84ab4017c1000089e2c4b06e331fe34c9e120836
Vlad Rakhmanin <91677083+toadkarter@users.noreply.github.com>
2022-08-13 18:33:02 +0100

    added frontend again

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+import './App.css';
+
+const App = () => {
+  const [userName, setUserName] = useState<string>("")
+
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/UserInput.tsx
@@ -0,0 +1,30 @@
+import {Dispatch, SetStateAction} from "react";
+import {Button, TextField} from "@mui/material";
+
+interface Props {
+  userName: string;
+  setUserName: Dispatch<SetStateAction<string>>;
+}
+
+//
+// Animation Bug Fix Credit:
+// https://stackoverflow.com/questions/55647969/how-to-get-one-elements-to-slide-in-while-the-other-slides-out-using-react-and
+
+const UserInput = ({userName, setUserName}: Props): JSX.Element => {
+  return <div>
+    <TextField
+      id="outlined-basic"
+      label="Outlined"
+      variant="outlined"
+      defaultValue={userName}
+    />
+    <Button
+      onClick={() => setUserName}
+    >
+
+    </Button>
+
+  </div>
+};
+
+export default UserInput;
